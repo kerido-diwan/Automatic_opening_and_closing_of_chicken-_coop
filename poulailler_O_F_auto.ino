de5b6fa5320429a8bd0681dfeca09695133488a8
Mael k <wows.moomoo@gmail.com>
2025-09-07 19:01:35 +0200

    V0
    première version.

---
+++ poulailler_O_F_auto.ino
@@ -0,0 +1,52 @@
+#define captlum A0
+#define led 8
+#define in1 10
+#define in2 11 
+int UP = 1;
+int DOWN = 0;
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(captlum, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+int lecture = analogRead(captlum);
+Serial.println(lecture);
+  if (analogRead(captlum) >600 & DOWN == 1){
+    moteur_up();
+    
+  }
+  if (analogRead(captlum) < 300 & UP == 1 ){
+    moteur_down();
+  }
+
+
+}
+
+void moteur_up() {
+  digitalWrite(in1, 1);
+  digitalWrite(in2, 0);
+  delay(1000);
+  digitalWrite(in1, 0);
+  digitalWrite(in2, 0);
+  UP = 1;
+  DOWN =0;
+  while(analogRead(captlum)>600);
+  delay(3000);
+}
+
+void moteur_down() {
+  digitalWrite(in1, 0);
+  digitalWrite(in2, 1);
+  delay(1000);
+  digitalWrite(in1, 0);
+  digitalWrite(in2, 0);
+  UP = 0;
+  DOWN = 1;
+  while(analogRead(captlum)<300);
+  delay(3000);
+  
+}
