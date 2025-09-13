397606e7d044139215b46f1d4f5e878b8ac9b0b4
Mael k <wows.moomoo@gmail.com>
2025-09-13 20:30:29 +0200

    Add files via upload
    cleaning du code et ajout de commentaires.

--- poulailler_O_F_auto.ino
+++ poulailler_O_F_auto.ino
@@ -1,40 +1,41 @@
-#define photo_res A0
+#define sunlight A0
 #define led 8
+// in1 et in2 sont les pins sur la carte L298N.A mettre sur pin PWN (logo ~).
 #define in1 10
-#define in2 11 
-int UP = 1;
-int DOWN = 0;
-
+#define in2 11
+// up et down son ici pour vérifier que le moteur ne s'ouvre/ferme pas 2 fois. (risque de casse).
+int up = 1;
+int down = 0;
 
 void setup() {
   Serial.begin (9600);
-  pinMode(photo_res, INPUT);
-  pinMode(led, OUTPUT);
+  pinMode(sunlight, INPUT);
+  pinMode(led, OUTPUT); 
 }
 
 void loop() {
-int lecture = analogRead(photo_res);
-Serial.println(lecture);
-  if (analogRead(photo_res) >600 & DOWN == 1){
+//Pour voir les valeurs de luminosité et donc faire son réglage personnel.
+int lecture = analogRead(sunlight);
+  Serial.println(lecture);
+//Automatisme d'ouverture.
+  if (analogRead(sunlight) >600 && down == 1){
     moteur_up();
-    
   }
-  if (analogRead(photo_res) < 300 & UP == 1 ){
+  if (analogRead(sunlight) < 300 && up == 1 ){
     moteur_down();
   }
-
-
 }
-
+// Ici les deux boucle pour l'ouverture et la fermeture.
+//Pour les valeurs et le fonctionement de in1 et in2 se référer à la documentation.
 void moteur_up() {
   digitalWrite(in1, 1);
   digitalWrite(in2, 0);
   delay(1000);
   digitalWrite(in1, 0);
   digitalWrite(in2, 0);
-  UP = 1;
-  DOWN =0;
-  while(analogRead(photo_res)>600);
+  up = 1;
+  down =0;
+  while(analogRead(sunlight)>600);
   delay(3000);
 }
 
@@ -44,9 +45,8 @@ void moteur_down() {
   delay(1000);
   digitalWrite(in1, 0);
   digitalWrite(in2, 0);
-  UP = 0;
-  DOWN = 1;
-  while(analogRead(photo_res)<300);
+  up = 0;
+  down = 1;
+  while(analogRead(sunlight)<300);
   delay(3000);
-  
 }
